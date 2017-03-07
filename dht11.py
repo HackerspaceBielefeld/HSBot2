a9c49cd04602a20730443953a1797a0929b2377d
HSBot2b <git@electronicfreak.de>
2017-03-07 13:11:06 +0100

    dht11 prüft nun 5 mal und rechet das mittel aus das sorgt für weniger seltsame werte

--- dht11.py
+++ dht11.py
@@ -14,17 +14,24 @@ mqtttopt = 'sensors/temperatur/innen'
 mqtttoph = 'sensors/luftfeuchtigkeit/innen'
 host     = 'https://hackerspace-bielefeld.de/spacestatus/spacestatus.php'
 
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+loops = 5
+
+humidity = 0.0
+temperature = 0.0
+
+for i in range(loops):
+	h, t = Adafruit_DHT.read_retry(sensor, pin)
+	humidity = humidity + h
+	temperature = temperature + t
+	sleep(1.2)
+
+temperature = round(temperature / loops,1)
+humidity = round(humidity / loops,1)
 
 if humidity is not None and temperature is not None:
 	mospub.single(mqtttopt, payload=temperature, hostname=mqttserv)
 	sleep(1)
 	mospub.single(mqtttoph, payload=humidity, hostname=mqttserv)
-	print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+	print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 
 	urllib2.urlopen(host +"?temp_in="+ str(temperature) +"&humi_in="+ str(humidity)).read()
-
-	sys.exit(0)
-else:
-    print('Failed to get reading. Try again!')
-    sys.exit(1)
